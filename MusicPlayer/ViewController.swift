f1d077334c291efdb5ef94097b911f3910fec6e8
Jake Contreras <jakeccontreras@icloud.com>
2019-01-07 22:47:28 -0800

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Jake Contreras on 1/7/19.
+//  Copyright © 2019 Jake Contreras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
